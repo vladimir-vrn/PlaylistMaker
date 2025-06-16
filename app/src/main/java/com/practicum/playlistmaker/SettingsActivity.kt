92f5deb603b40333312eb19f30c5a3631d30e85c
vladimir-vrn <dvv_mail@mail.ru>
2025-06-16 21:15:38 +0300

    ДЗ Спринт №8

---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package com.practicum.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val imgLeftArrow = findViewById<ImageView>(R.id.img_left_arrow)
+        imgLeftArrow.setOnClickListener {
+            val intentMain = Intent(this, MainActivity::class.java)
+            startActivity(intentMain)
+        }
+    }
+}
