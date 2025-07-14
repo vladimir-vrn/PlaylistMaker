e623af027297930632745136d7c9e7352a2fab9b
vladimir-vrn <dvv_mail@mail.ru>
2025-07-14 22:12:49 +0300

    ДЗ Спринт №10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }
