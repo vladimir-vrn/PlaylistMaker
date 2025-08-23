d4a59f079684cfa7d8bf5430af62888a46afe0e0
vladimir-vrn <dvv_mail@mail.ru>
2025-08-23 17:24:56 +0300

    ДЗ Спринт №13

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -6,6 +6,12 @@ data class Track(
     val trackId: Long,
     val trackName: String,
     val artistName: String,
-    @SerializedName("trackTimeMillis") val trackTime: String,
-    val artworkUrl100: String
-)
+    @SerializedName("trackTimeMillis") val trackTime: Long,
+    val artworkUrl100: String,
+    val collectionName: String,
+    val releaseDate: String,
+    val primaryGenreName: String,
+    val country: String,
+) {
+    fun getCoverArtwork() = artworkUrl100.replaceAfterLast('/',"512x512bb.jpg")
+}
